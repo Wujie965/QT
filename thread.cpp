2ab62080955a782c78d6df8afb020773350ac775
Wujie965 <mjzhutianxiu@163.com>
2020-11-23 18:32:55 +0800

    add thread.cpp and thread.h file!

---
+++ thread.cpp
@@ -0,0 +1,5 @@
+#include "thread.h"
+
+TestThread::TestThread(QWidget* parent): QWidget (parent){}
+
+TestThread::~TestThread(){}
---
+++ thread.h
@@ -0,0 +1,14 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <QWidget>
+
+class TestThread:  public QWidget{
+    Q_OBJECT
+public:
+    TestThread(QWidget* parent = nullptr);
+    ~TestThread();
+private:
+};
+
+#endif // THREAD_H
